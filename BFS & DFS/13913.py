862ab58e59c8004800bbdb1ef30c247d724c2e59
Song jinho <songjinho776@gmail.com>
2023-06-27 22:04:45 +0900

    코딩테스트 기초 - BFS

--- BFS & DFS/13913.py
+++ BFS & DFS/13913.py
@@ -9,16 +9,25 @@ def bfs():
 
         if x == K:
             print(dist[x])
+            path(x)
             break
 
         for nx in (x-1, x+1, x*2):
             if 0 <= nx <= 100000 and not dist[nx]:
                 dist[nx] = dist[x] + 1
                 queue.append(nx)
+                move[nx] = x
 
-
+def path(x):
+    arr = []
+    temp = x
+    for _ in range(dist[x] + 1):
+        arr.append(temp)
+        temp = move[temp]
+    print(''.join(map(str, arr[::-1])))
 
 N, K = map(int, input().split())
 dist = [0] * 100001
+move = [0] * 100001
 
 bfs()
