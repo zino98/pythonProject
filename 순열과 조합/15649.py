2629d99a899b6ebd1d76d4feeba76aeda8f9571f
Song jinho <songjinho776@gmail.com>
2023-09-21 20:18:09 +0900

    코딩테스트 연습 - 순열과 조합

---
+++ 순열과 조합/15649.py
@@ -0,0 +1,12 @@
+from itertools import permutations
+
+n, m = map(int, input().split())
+items = []
+for i in range(1, n+1):
+    items.append(i)
+
+for i in permutations(items, m):
+    if len(i) == 1:
+        print(i[0])
+    else:
+        print(*i, sep=' ')
