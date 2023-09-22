6db34f26752e758f8849c36e2c32bcfcc035a89f
Song jinho <songjinho776@gmail.com>
2023-09-22 21:20:07 +0900

    코딩테스트 연습 - DP

---
+++ DP/14501.py
@@ -0,0 +1,14 @@
+import sys
+N = int(input())
+
+schedule = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+dp = [0 for _ in range(N+1)]
+
+for i in range(N):
+    for j in range(i+schedule[i][0], N+1):
+        if dp[j] < dp[i] + schedule[i][1]:
+            dp[j] = dp[i] + schedule[i][1]
+
+
+print(dp[-1])
