35407f269bac81e6c73567d0fb9a6ba134643e7d
Song jinho <songjinho776@gmail.com>
2023-10-05 21:07:22 +0900

    코딩테스트 연습 - 브루트포스

--- 브루트포스/12100.py
+++ 브루트포스/12100.py
@@ -25,6 +25,62 @@ def move(graph, dir):
                     else:
                         edge -= 1
                         graph[i][edge] = tmp
+
+    if dir == 1:
+        edge = 0
+
+        for i in range(N):
+            for j in range(1,N):
+                if graph[i][j]:
+                    tmp = graph[i][j]
+                    graph[i][j] = 0
+
+                    if graph[i][edge] == tmp:
+                        graph[i][edge] = 2 * tmp
+                        edge += 1
+
+                    elif graph[i][edge] == 0:
+                        graph[i][edge] = tmp
+
+                    else:
+                        edge += 1
+                        graph[i][edge] = tmp
+
+
+    elif dir == 2:  # 남쪽
+        for j in range(N):
+            top = N - 1
+            for i in range(N - 2, -1, -1):
+                if board[i][j]:
+                    tmp = board[i][j]
+                    board[i][j] = 0
+                    if board[top][j] == 0:
+                        board[top][j] = tmp
+                    elif board[top][j] == tmp:
+                        board[top][j] = tmp * 2
+                        top -= 1
+                    else:
+                        top -= 1
+                        board[top][j] = tmp
+
+    else:
+        for j in range(N):
+            top = 0
+            for i in range(1, N):
+                if board[i][j]:
+                    tmp = board[i][j]
+                    board[i][j] = 0
+                    if board[top][j] == 0:
+                        board[top][j] = tmp
+                    elif board[top][j] == tmp:
+                        board[top][j] = tmp * 2
+                        top += 1
+                    else:
+                        top += 1
+                        board[top][j] = tmp
+
+    return board
+
 def dfs(board, cnt):
     global answer
     if cnt == 5:
