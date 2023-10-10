788fc2a1d8ced12e7bc881c88687cb3d0daa3427
Song jinho <songjinho776@gmail.com>
2023-10-10 21:06:38 +0900

    코딩테스트 연습 - 삼성 SW 기출

---
+++ 삼성 SW 기출문제/14499.py
@@ -0,0 +1,43 @@
+N, M, x, y, K = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+move = list(map(int, input().split()))
+
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
+dice = [0,0,0,0,0,0]
+
+def turn(dir):
+    a, b, c, d, e, f = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
+    if dir == 1: #동
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = d, b, a, f, e, c
+
+    elif dir == 2: #서
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = c, b, f, a, e, d
+
+    elif dir == 3: #북
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = e, a, c, d, f, b
+
+    else:
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = b, f, c, d, a, e
+
+nx, ny = x, y
+for i in move:
+    nx = nx + dx[i-1]
+    ny = ny + dy[i-1]
+
+    if nx < 0 or nx >= N or ny < 0 or ny >= M:
+        nx -= dx[i-1]
+        ny -= dy[i-1]
+        continue
+
+    turn(i)
+
+    if board[nx][ny] == 0:
+        board[nx][ny] = dice[-1]
+
+    else:
+        dice[-1] = board[nx][ny]
+        board[nx][ny] = 0
+
+    print(dice[0])
+
